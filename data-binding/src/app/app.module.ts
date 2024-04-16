ab13b262a7c3803a7bcca1b709a7bb6160ff50a4
Mitilene Merabe <56640902+mitilene@users.noreply.github.com>
2024-04-15 22:00:04 -0300

    Atualização geral de projeto

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent implements OnInit {
+  url: string = "http://loiane.com";
+  cursoAngular: boolean = true;
+  urlImage: string = "http://lorempixel.com.br/400/200/nature";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+}
